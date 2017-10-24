3bb33924fffa417249ddb1408ff2d3b628f1bf1a
miguelmoresalcedo <spyrone1@gmail.com>
2017-10-24 12:48:11 +0200

    new

--- 5. MenuFicheros/src/MenuFich.java
+++ 5. MenuFicheros/src/MenuFich.java
@@ -17,7 +17,7 @@ public class MenuFich {
 		// TODO Auto-generated method stub
 		
 		Scanner scan = new Scanner(System.in);
-		String nombre = scan.nextLine();
+		String nombre = scan.nextLine(); //el .nexLine() sirve para leer en este caso un cadena, y luego puedes guardarlo en una variable o no.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Selecciona la opcion que quieres: 1. Crea Fichero, 2. Muestra informacion, 3. Muestra longitud, 4. Comprueba si existe, 5. Borra fichero, 6. Salir");
 		int numero = sc.nextInt();
@@ -42,9 +42,9 @@ public class MenuFich {
 			
 			catch (IOException e)  
 			{
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
 			
 			break;
 		case 2:
@@ -89,6 +89,10 @@ public class MenuFich {
 			fichero = new File(nombre + ".txt");
 			fichero.delete();
 			 break;
+			 
+		case 6:
+			System.exit(0);
+			break;
 		
 		}
 	}
